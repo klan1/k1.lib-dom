e6f0ab4318157cfe99beac1205833053ce3f8b23
Alejandro Trujillo J. <contacto@klan1.com>
2016-10-23 14:57:33 -0500

    - improvements

--- src/html/classes-tag.php
+++ src/html/classes-tag.php
@@ -2211,6 +2211,24 @@ class pre extends tag {
 
 }
 
+class iframe extends tag {
+
+    use append_shotcuts;
+
+    function __construct($src, $class = NULL, $id = NULL) {
+        parent::__construct("iframe", IS_NOT_SELF_CLOSED);
+        $this->set_value($src);
+        $this->set_class($class);
+        $this->set_id($id);
+    }
+
+    public function set_value($value, $append = FALSE) {
+        $this->set_attrib("src", $value);
+        return $this;
+    }
+
+}
+
 class button extends tag {
 
     use append_shotcuts;
