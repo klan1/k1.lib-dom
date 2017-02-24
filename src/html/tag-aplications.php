59f231802adf86db621be181f9bb482144dbbda1
Alejandro Trujillo J. <contacto@klan1.com>
2017-02-24 18:42:08 -0500

    - updates

--- src/html/tag-aplications.php
+++ src/html/tag-aplications.php
@@ -964,17 +964,17 @@ class grid_row extends \k1lib\html\div {
         }
 
         for ($col = 1; $col <= $num_cols; $col++) {
-            $this->cols[$col] = $this->append_cell($col);
+            $this->num_cols[$col] = $this->append_cell($col);
         }
     }
 
     /**
      * @param integer $col_number
-     * @return \k1lib\html\tag
+     * @return \k1lib\html\foundation\grid_cell
      */
     public function col($col_number) {
-        if (isset($this->cols[$col_number])) {
-            return $this->cols[$col_number];
+        if (isset($this->num_cols[$col_number])) {
+            return $this->num_cols[$col_number];
         }
     }
 
@@ -1011,25 +1011,26 @@ class grid extends \k1lib\html\div {
     /**
      * @var grid_cell[]
      */
-    protected $rows;
-    protected $cols;
+    protected $rows = [];
+    protected $num_rows;
+    protected $num_cols;
 
     public function __construct($num_rows, $num_cols, \k1lib\html\tag $parent = NULL) {
         $this->parent = $parent;
 
-        $this->rows = $num_rows;
-        $this->cols = $num_cols;
+        $this->num_rows = 0;
+        $this->num_cols = $num_cols;
 
         if (empty($this->parent)) {
             parent::__construct();
             for ($row = 1; $row <= $num_rows; $row++) {
-                $this->rows[$row] = $this->append_row($num_cols, $row, $this);
+                $this->append_row($num_cols, $row, $this);
             }
         } else {
 //            $this->append_to($this->parent);
             $this->link_value_obj($parent);
             for ($row = 1; $row <= $num_rows; $row++) {
-                $this->rows[$row] = $this->append_row($num_cols, $row, $this->parent);
+                $this->append_row($num_cols, $row, $this->parent);
             }
             return $parent;
         }
@@ -1053,9 +1054,10 @@ class grid extends \k1lib\html\div {
      */
     public function append_row($num_cols = NULL, $grid_row = NULL, $parent = NULL) {
         if ($num_cols === NULL) {
-            $num_cols = $this->cols;
+            $num_cols = $this->num_cols;
         }
         $row = new grid_row($num_cols, $grid_row, $parent);
+        $this->rows[++$this->num_rows] = $row;
         return $row;
     }
 
