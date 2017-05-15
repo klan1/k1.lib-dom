c1d72ae3e1fb9da1067ad4d0ed11fb6f3524036d
Alejandro Trujillo J. <contacto@klan1.com>
2017-05-15 18:54:04 -0500

    - entrega para revision final

--- src/html/classes-tag.php
+++ src/html/classes-tag.php
@@ -1798,6 +1798,25 @@ class script extends tag {
 
 }
 
+class style extends tag {
+
+    use append_shotcuts;
+
+    /**
+     * Create a SCRIPT html tag with VALUE as data. Use $style->set_value($crs) 
+     * for load a file.
+     * @param String $class
+     * @param String $id
+     */
+    function __construct($style = NULL) {
+        parent::__construct("style", FALSE);
+        if (!empty($style)) {
+            $this->set_value($style);
+        }
+    }
+
+}
+
 class small extends tag {
 
     use append_shotcuts;
